7f3b878e86a64d524b98fd069c2b0dd27736cc7a
Nadav Ivgi <nadav@shesek.info>
2017-02-14 10:05:39 +0200

    Fix default behaviour when no Accept header is specified

--- server.js
+++ server.js
@@ -6,12 +6,14 @@ const app = require('express')()
 app.disable('x-powered-by')
 app.use(require('morgan')('dev'))
 
+let redirect
 app.get('/:addr([13][0-9a-zA-Z]+)/:amount([0-9.]+)/:currency([A-Z]{3})', (req, res, next) => {
   toBTC(req.params.amount, req.params.currency, iferr(next, amount =>
     res.format({
-      default: _ => res.redirect(302, formatURL(req.params.addr, amount, req.query))
+      'text/html': redirect = _ => res.redirect(302, formatURL(req.params.addr, amount, req.query))
     , 'application/bitcoin-paymentrequest': _ => res.type('application/bitcoin-paymentrequest')
                                              .send(makePaymentReq(req.params.addr, amount, req.query))
+    , default: redirect
     })
   ))
 })
