7e305f7616fed29ebd62cafcfa89f78f934a9dea
Nadav Ivgi <nadav@shesek.info>
2017-02-14 14:23:31 +0200

    styling

--- server.js
+++ server.js
@@ -2,20 +2,20 @@ const iferr  = require('iferr')
     , except = require('except')
     , { toBTC, formatURL, makePaymentReq } = require('./util')
 
+    , CTYPE = 'application/bitcoin-paymentrequest'
+
 const app = require('express')()
-app.disable('x-powered-by')
 app.use(require('morgan')('dev'))
 
 let redirect
-app.get('/:addr([13][0-9a-zA-Z]+)/:amount([0-9.]+)/:currency([A-Z]{3})', (req, res, next) => {
-  toBTC(req.params.amount, req.params.currency, iferr(next, amount =>
+app.get('/:addr([13][0-9a-zA-Z]+)/:amount([0-9.]+)/:currency([A-Z]{3})', ({ params, query }, res, next) =>
+  toBTC(params.amount, params.currency, iferr(next, amount =>
     res.format({
-      'text/html': redirect = _ => res.redirect(302, formatURL(req.params.addr, amount, req.query))
-    , 'application/bitcoin-paymentrequest': _ => res.type('application/bitcoin-paymentrequest')
-                                             .send(makePaymentReq(req.params.addr, amount, req.query))
+      'text/html': redirect = _ => res.redirect(302, formatURL(params.addr, amount, query))
+    , [CTYPE]: _ => res.type(CTYPE).send(makePaymentReq(params.addr, amount, query))
     , default: redirect
     })
   ))
-})
+)
 
 app.listen(process.env.PORT || 3000, _ => console.log('HTTP server running.'))
