06a78875a001ff26c473889fefaf5e3db27392e5
Nadav Ivgi <nadav@shesek.info>
2017-02-14 02:44:14 +0200

    Add initial support for BIP70/BIP71 payment requests

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const iferr  = require('iferr')
     , except = require('except')
-    , { toBTC, formatURL } = require('./util')
+    , { toBTC, formatURL, makePaymentReq } = require('./util')
 
 const app = require('express')()
 
@@ -10,9 +10,13 @@ app.get('/:addr([13][0-9a-zA-Z]+)', (req, res, next) => {
   if (!/^[A-Z]{3}$/.test(req.query.currency) || isNaN(+req.query.amount))
     return res.status(400).send('Invalid parameters')
 
-  const extra = except(req.query, 'amount', 'currency')
   toBTC(req.query.amount, req.query.currency, iferr(next, amount =>
-    res.redirect(302, formatURL(req.params.addr, Object.assign(extra, { amount })))))
+    res.format({
+      default: _ => res.redirect(302, formatURL(req.params.addr, Object.assign(except(req.query, 'currency'), { amount })))
+    , 'application/bitcoin-paymentrequest': _ => res.type('application/bitcoin-paymentrequest')
+                                             .send(makePaymentReq(req.params.addr, amount))
+    })
+  ))
 })
 
 app.listen(process.env.PORT || 3000, _ => console.log('HTTP server running.'))
--- util.js
+++ util.js
@@ -1,14 +1,28 @@
 const request = require('superagent')
     , iferr   = require('iferr')
     , qs      = require('querystring')
+    , movedec = require('move-decimal-point')
+    , { PaymentRequest, PaymentDetails } = require('bcoin').bip70
 
 const getRate = (currency, cb) =>
   request('https://apiv2.bitcoinaverage.com/indices/local/ticker/BTC'+currency)
     .end(iferr(cb, resp => cb(null, +resp.body.last)))
 
-const toBTC = (amount, currency='USD', cb) => getRate(currency, iferr(cb,
+const toSatoshis = btcs => movedec(btcs, -8)
+
+const toBTC = (amount, currency, cb) => getRate(currency, iferr(cb,
   rate => cb(null, (amount/rate).toFixed(8))))
 
 const formatURL = (addr, opt) => `bitcoin:${ addr }?${ qs.stringify(opt) }`
 
-module.exports = { toBTC, formatURL }
+const makePaymentReq = (addr, amount) =>
+  PaymentRequest.fromOptions({
+    version: 1
+  , paymentDetails: PaymentDetails.fromOptions({
+      network: 'main'
+    , time: Date.now()/1000 | 0
+    , outputs: [ { address: addr, amount: toSatoshis(amount)  } ]
+    })
+  }).toRaw()
+
+module.exports = { toBTC, formatURL, makePaymentReq }
